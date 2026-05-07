094c18e26a9d77be52bc79243205aca3b3c00d66
Dani Arifin <danithesillyfem@gmail.com>
2026-05-07 19:34:04 +0700

    Update constants.py

--- opencore_legacy_patcher/constants.py
+++ opencore_legacy_patcher/constants.py
@@ -28,7 +28,7 @@ class Constants:
 
         # OpenCore Versioning
         # https://github.com/acidanthera/OpenCorePkg
-        self.opencore_version: str = "1.0.5"
+        self.opencore_version: str = "1.0.7"
 
         # Kext Versioning
         ## Acidanthera
